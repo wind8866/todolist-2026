4111b21b4ad924c9c292055e5b65506ba0ab1e7d
wind8866 <liuzhen1010xyz@gmail.com>
2026-05-09 20:12:21 +0800

    feat: 删掉自带的模板

--- packages/vue2026/src/App.vue
+++ packages/vue2026/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue';
 </script>
 
 <template>
-  <HelloWorld />
+  <TodoList />
 </template>
